0c59e1178dbf535e6bc3c331828b10a5b0215e1b
odileyandi-sir <87127794+odileyandi-sir@users.noreply.github.com>
2021-12-18 07:26:10 +0530

    Create LogisticRegression.R

---
+++ LogisticRegression.R
@@ -0,0 +1,6 @@
+input <- mtcars[,c("am","cyl","hp","wt")]
+print(head(input))
+#create regression model:
+input <- mtcars[,c("am","cyl","hp","wt")]
+am.data = glm(formula = am ~ cyl + hp + wt, data = input, family = binomial)
+print(summary(am.data))
